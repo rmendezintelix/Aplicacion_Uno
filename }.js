a17408a0bacacda68bf026165cfcd86fcb6fe233
rmendezintelix <noreply>
2017-10-11 12:05:56 +0000

    Add module }

---
+++ }.js
@@ -0,0 +1,15 @@
+/**
+ * @module //}
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class }
+ * @extends Montage
+ */
+exports.} = Montage.specialize(/** @lends }# */ {
+    constructor: {
+        value: function }() {
+            this.super();
+        }
+    }
+});
